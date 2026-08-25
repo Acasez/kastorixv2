be377655046235d3a7d0a29a51f2b8b99315878c
Acasez <edvin.sanne@gmail.com>
2026-08-25 13:45:38 +0200

    Working localhost page

--- src/Subpages/RPGOverview.tsx
+++ src/Subpages/RPGOverview.tsx
@@ -1,3 +1,49 @@
+import "../CSS/RowStructure.css";
+
 export default function RPGOverview() {
-  return <>Homepage for Kastorix</>;
+  return (
+    <>
+      <header>
+        <h3 className="portfolio-title">Kastorix</h3>
+        <p className="portfolio-subtitle">TTRPG Overview</p>
+      </header>
+
+      <section className="portfolio-row reverse">
+        {" "}
+        {/* Overview */}
+        <div className="portfolio-text">
+          <img
+            className="floated-image"
+            src="/images/Maps/Kastorix.jpg"
+            alt="Kastorix World Map"
+          />
+          <p className="portfolio-description">
+            Kastorix is the name of both the TTRPG and the world it's set in.
+            It's D20 based system heavily inspired by Pathfinder 2e.
+            <br />
+            <br />
+            In Kastorix magic is commonplace and mages are one of the main
+            sources of power for nations. There are no classes, instead options
+            come from feats and spells. In a sense, everyone is a mage, though
+            there is considerable difference between different mages from the
+            feats chosen. To start making a character players pick their
+            species, then pick up feats and spells
+            <br />
+            <br />
+            The world and magic of Kastorix is inspired by magitech worlds like
+            Eberron, Ravnica, Runeterra and Avishkar, as well as progression
+            fantasy worlds and stories like Arcane Ascension, Mother of Learning
+            and the Journals of Evander Tailor.
+            <br />
+            <br />
+            By design all powerful characters are magic users and rely on their
+            auras instead of superhuman durability, though they need not be
+            exactly traditional mages. There is room to build tinkering
+            artificers, heavily armored magic knights and rogues with magical
+            tricks
+          </p>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -8,103 +8,15 @@ export default function MainHeader() {
         <ul>
           <li>
             <NavLink to="/" end>
-              <u>Edvin Skogsholm Sanne</u>
+              <u>RPG Overview</u>
             </NavLink>
           </li>
           <li>
-            <NavLink to="/EdvinsNestedTooltips">
-              Edvin's Nested Tooltips
-            </NavLink>
+            <NavLink to="/CharacterSheet">CharacterSheet</NavLink>
           </li>
           <li>
-            <NavLink to="/Amsvartne">Amsvartne</NavLink>
+            <NavLink to="/DMScreen">DM Screen</NavLink>
           </li>
-          <li>
-            <a href="#">Mariestad Board Game ▼</a>
-            <div className="dropdown_menu">
-              <ul>
-                <li>
-                  <NavLink to="/MariestadClimateGame">
-                    Mariestad Board Game
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/MariestadDigitalAdaptation">
-                    Mariestad Digital Adaptation
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <a href="#">Ion Internship ▼</a>
-            <div className="dropdown_menu">
-              <ul>
-                <li>
-                  <NavLink to="/IonInternship">Ion Internship</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/HighFrontierTutorial">High Frontier</NavLink>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <NavLink to="/CityState">City State</NavLink>
-          </li>
-          <li>
-            <NavLink to="/ToHelAndBack">To Hel and Back</NavLink>
-          </li>
-          <li>
-            <NavLink to="/landOfTheArcane">Land of the Arcane</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Kastorix">Kastorix</NavLink>
-          </li>
-          <li>
-            <NavLink to="/AboutMe">About Me</NavLink>
-          </li>
-          {/* <li>
-            <a href="#">About Me ▼</a>
-            <div className="dropdown_menu">
-              <ul>
-                <li>
-                  <NavLink to="/AboutMe">About Me</NavLink>
-                </li>
-                <li>
-                  <a href="#">🞀 CV's</a>
-                  <div className="dropdown_menu">
-                    <ul>
-                      <li>
-                        <NavLink to="/EnglishCV">English</NavLink>
-                      </li>
-                      <li>
-                        <a href="swedishCV.html">Swedish</a>
-                      </li>
-                    </ul>
-                  </div>
-                </li>
-                <li>
-                  <a href="#">🞀 Fan Content</a>
-                  <div className="dropdown_menu">
-                    <ul>
-                      <li>
-                        <NavLink to="/AOW4Tomes">
-                          Age of Wonders 4 Tomes
-                        </NavLink>
-                      </li>
-                      <li>
-                        <a href="myherosnap.html">My Hero Snap</a>
-                      </li>
-                      <li>
-                        <a href="meridianSunsets.html">Meridian Sunsets</a>
-                      </li>
-                    </ul>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </li> */}
         </ul>
       </nav>
     </>
