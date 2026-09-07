eb608d5a03a672db081d845701d18bce794ce1b2
Acasez <edvin.sanne@gmail.com>
2026-09-07 21:28:46 +0200

    Can see saving throws

--- src/components/CharacterSheet/CoreStatsSection.tsx
+++ src/components/CharacterSheet/CoreStatsSection.tsx
@@ -1,37 +1,11 @@
-/* CoreStatsSection.tsx; */
-/* import { getProficiency } from "../../constants/Proficiency";
-import { useCharacter } from "../../contexts/CharacterContext";
-import ProficiencyMarker from "./ProficiencyMarker"; */
+import SavingThrowDisplay from "./SavingThrowDisplay";
 import StatsGrid from "./StatsGrid";
 
 export default function CoreStatSection() {
-  /* const { character, updateCharacter } = useCharacter(); */
   return (
-    <div>
+    <div className="flex flex-col">
       <StatsGrid />
-      {/* <div className="flex items-center justify-center gap-3">
-        {[
-          { name: "Fortitude", stat: "PHY" },
-          { name: "Reflex", stat: "DEX" },
-          { name: "Will", stat: "WIL" },
-        ].map((save) => {
-          const tierName =
-            character.saveProficiencies[save.name] ?? "Untrained";
-          const tier = getProficiency(tierName);
-          const stat = character.baseStats[save.stat] ?? 0;
-
-          return (
-            <div
-              key={save.name}
-              className="flex items-center gap-1.5 rounded border border-blue-300 px-2 py-1 text-sm"
-            >
-              <span className="text-blue-300">{save.name}</span>
-              <ProficiencyMarker skillName={save.name} />
-              <span>{clampToDice(save.bonus)}</span>
-            </div>
-          );
-        })}
-      </div> */}
+      <SavingThrowDisplay />
     </div>
   );
 }
---
+++ src/components/CharacterSheet/SavingThrowDisplay.tsx
@@ -0,0 +1,36 @@
+/* CoreStatsSection.tsx; */
+import { getProficiency } from "../../constants/Proficiency";
+import { STRING_TO_STATKEY } from "../../constants/StatKey";
+import { useCharacter } from "../../contexts/CharacterContext";
+import ProficiencyMarker from "./ProficiencyMarker";
+
+export default function SavingThrowDisplay() {
+  const { character } = useCharacter();
+  return (
+    <div>
+      <div className="flex items-center justify-center gap-3">
+        {[
+          { name: "Fortitude", stat: "PHY" },
+          { name: "Reflex", stat: "DEX" },
+          { name: "Will", stat: "WIL" },
+        ].map((save) => {
+          const tierName =
+            character.saveProficiencies[save.name] ?? "Untrained";
+          const tier = getProficiency(tierName);
+          const stat = character.baseStats[STRING_TO_STATKEY[save.stat]] ?? 0;
+          const bonus = stat + tier.bonus;
+          return (
+            <div
+              key={save.name}
+              className="flex items-center gap-1.5 rounded border border-blue-300 px-2 py-1 text-sm"
+            >
+              <span className="text-blue-300">{save.name}</span>
+              <ProficiencyMarker skillName={save.name} />
+              <span>{bonus}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
