135da3333f4b8cab5bd5cb6d63812c78eb992e8f
Acasez <edvin.sanne@gmail.com>
2026-09-07 08:51:15 +0200

    Tested context

--- src/components/CharacterSheet/LevelSidebar.tsx
+++ src/components/CharacterSheet/LevelSidebar.tsx
@@ -1,12 +1,15 @@
 import CharacterPanel from "./CharacterPanel";
 import LevelCard from "./LevelCard";
 import SaveLoadButtons from "./SaveLoadButtons";
+import { useCharacter } from "./useCharacter";
 
 export default function LevelSidebar() {
   const levels = Array.from({ length: 21 }, (_, i) => i); // 0 to 20
+  const { characterName } = useCharacter();
 
   return (
     <div className="w-80 p-4 bg-gray-100 rounded-lg shadow-lg overflow-y-auto max-h-screen">
+      <h1>{characterName}</h1> {/* Test, works */}
       <SaveLoadButtons />
       <CharacterPanel />
       <div className="space-y-3">
