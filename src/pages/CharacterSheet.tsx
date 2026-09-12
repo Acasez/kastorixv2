5e141245df12368d3402fe9f42b57ff2c53a3a8f
Acasez <edvin.sanne@gmail.com>
2026-09-12 18:55:08 +0200

    Fixed import

--- src/pages/CharacterSheet.tsx
+++ src/pages/CharacterSheet.tsx
@@ -1,7 +1,7 @@
 import ActionSection from "../components/CharacterSheet/ActionSection/ActionSection";
 import CoreStatSection from "../components/CharacterSheet/CoreStatSection/CoreStatsSection";
-import LevelSidebar from "../components/CharacterSheet/LevelSidebar";
 import OtherStatSection from "../components/CharacterSheet/OtherInfoSection/OtherStatSection";
+import LevelSidebar from "../components/CharacterSheet/Sidebar/LevelSidebar";
 import SkillsTable from "../components/CharacterSheet/SkillTable";
 import { CharacterProvider } from "../contexts/CharacterProvider";
 
