be4b35913e19f7ddad5a36b625b7329319dd4f69
Acasez <edvin.sanne@gmail.com>
2026-09-14 21:07:47 +0200

    Moved species specific to other modal

--- src/components/ModalWrapper.tsx
+++ src/components/ModalWrapper.tsx
@@ -1,6 +1,4 @@
-import species from ".././JSON/species.json";
-import { useState } from "react";
-import TraitRow from "./TraitRow";
+import SpeciesModal from "./SpeciesModal";
 
 interface ModalWrapperProps {
   openModalLabel: string;
@@ -11,8 +9,6 @@ export default function ModalWrapper({
   openModalLabel,
   closeModal,
 }: ModalWrapperProps) {
-  const [selectedSpecies, setSelectedSpecies] = useState(species[0]); // Default to first species
-
   return (
     <>
       <div
@@ -23,78 +19,10 @@ export default function ModalWrapper({
           className="bg-white rounded-lg p-8 max-w-6xl w-11/12 h-[85vh] overflow-auto shadow-2xl border border-gray-300"
           onClick={(e) => e.stopPropagation()}
         >
-          <h2 className="text-3xl font-bold text-gray-800 mb-4 border-b-2 border-striking">
-            {openModalLabel}
-          </h2>
-          <div className="flex flex-row gap-3">
-            {/* Species Selection Panel */}
-            <div className="flex flex-col w-40 gap-1">
-              {species.map((speciesItem) => (
-                <button
-                  key={speciesItem.name}
-                  className={`text-xl border-2 ${
-                    selectedSpecies?.name === speciesItem.name
-                      ? "border-purple-500 bg-purple-100"
-                      : "border-amber-200 bg-bg-rules"
-                  } text-text-dark`}
-                  onClick={() => setSelectedSpecies(speciesItem)}
-                >
-                  {speciesItem.name}
-                </button>
-              ))}
-            </div>
-
-            {/* Species Info Panel */}
-            <div className="border-amber-200 border-2 p-3 flex-1">
-              {selectedSpecies ? (
-                <>
-                  <h1 className="text-2xl underline">{selectedSpecies.name}</h1>
-
-                  <div className="grid grid-cols-2 gap-4 mt-4">
-                    <div>
-                      <p>
-                        <strong>Size:</strong> {selectedSpecies.size}
-                      </p>
-                      <p>
-                        <strong>Health:</strong> {selectedSpecies.health}
-                      </p>
-                      <p>
-                        <strong>Mana:</strong> {selectedSpecies.mana}
-                      </p>
-                    </div>
-                  </div>
-
-                  <div className="mt-4 space-y-4">
-                    <TraitRow
-                      traitName={selectedSpecies.traitOne}
-                      traitDescription={selectedSpecies.traitOneDesc}
-                    />
-                    <TraitRow
-                      traitName={selectedSpecies.traitTwo}
-                      traitDescription={selectedSpecies.traitTwoDesc}
-                    />
-                    <TraitRow
-                      traitName={selectedSpecies.traitThree}
-                      traitDescription={selectedSpecies.traitThreeDesc}
-                    />
-                    <TraitRow
-                      traitName={selectedSpecies.traitFour}
-                      traitDescription={selectedSpecies.traitFourDesc}
-                    />
-                  </div>
-
-                  <button
-                    className="bg-lime-300 p-1 rounded-md mt-4"
-                    onClick={() => closeModal()}
-                  >
-                    Select Species
-                  </button>
-                </>
-              ) : (
-                <p>Select a species from the left panel</p>
-              )}
-            </div>
-          </div>
+          <SpeciesModal
+            openModalLabel={openModalLabel}
+            closeModal={closeModal}
+          />
         </div>
       </div>
     </>
---
+++ src/components/SpeciesModal.tsx
@@ -0,0 +1,95 @@
+import species from "../JSON/species.json";
+import { useState } from "react";
+import TraitRow from "./TraitRow";
+
+interface ModalWrapperProps {
+  openModalLabel: string;
+  closeModal: () => void;
+}
+
+export default function SpeciesModal({
+  openModalLabel,
+  closeModal,
+}: ModalWrapperProps) {
+  const [selectedSpecies, setSelectedSpecies] = useState(species[0]); // Default to first species
+
+  return (
+    <>
+      <h2 className="text-3xl font-bold text-gray-800 mb-4 border-b-2 border-striking">
+        {openModalLabel}
+      </h2>
+      <div className="flex flex-row gap-3">
+        {/* Species Selection Panel */}
+        <div className="flex flex-col w-40 gap-1">
+          {species.map((speciesItem) => (
+            <button
+              key={speciesItem.name}
+              className={`text-xl border-2 ${
+                selectedSpecies?.name === speciesItem.name
+                  ? "border-purple-500 bg-purple-100"
+                  : "border-amber-200 bg-bg-rules"
+              } text-text-dark`}
+              onClick={() => setSelectedSpecies(speciesItem)}
+            >
+              {speciesItem.name}
+            </button>
+          ))}
+        </div>
+
+        {/* Species Info Panel */}
+        <div className="border-amber-200 border-2 p-3 flex-1">
+          {selectedSpecies ? (
+            <>
+              <h1 className="text-2xl underline">{selectedSpecies.name}</h1>
+
+              <div className="grid grid-cols-2 gap-4 mt-4">
+                <div>
+                  <p>
+                    <span className="font-bold">Size: </span>
+                    {selectedSpecies.size}
+                  </p>
+                  <p>
+                    <span className="font-bold">Health: </span>{" "}
+                    {selectedSpecies.health}
+                  </p>
+                  <p>
+                    <span className="font-bold">Mana: </span>{" "}
+                    {selectedSpecies.mana}
+                  </p>
+                </div>
+              </div>
+
+              <div className="mt-4 space-y-4">
+                <TraitRow
+                  traitName={selectedSpecies.traitOne}
+                  traitDescription={selectedSpecies.traitOneDesc}
+                />
+                <TraitRow
+                  traitName={selectedSpecies.traitTwo}
+                  traitDescription={selectedSpecies.traitTwoDesc}
+                />
+                <TraitRow
+                  traitName={selectedSpecies.traitThree}
+                  traitDescription={selectedSpecies.traitThreeDesc}
+                />
+                <TraitRow
+                  traitName={selectedSpecies.traitFour}
+                  traitDescription={selectedSpecies.traitFourDesc}
+                />
+              </div>
+
+              <button
+                className="bg-lime-300 p-1 rounded-md mt-4"
+                onClick={() => closeModal()}
+              >
+                Select Species
+              </button>
+            </>
+          ) : (
+            <p>Select a species from the left panel</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
