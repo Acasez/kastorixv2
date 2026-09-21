594bc3b4c5ddbd13c28e61fcbc2257fc36273b4c
Acasez <edvin.sanne@gmail.com>
2026-09-21 16:58:10 +0200

    Fixed weapon tooltips

--- src/components/CharacterSheet/ActionSection/WeaponComponent.tsx
+++ src/components/CharacterSheet/ActionSection/WeaponComponent.tsx
@@ -24,16 +24,41 @@ export default function WeaponComponent({
       ? Math.max(character.baseStats.DEX, character.baseStats.PHY)
       : character.baseStats.PHY;
 
+  const damageBonus = weaponTraits.includes("Ranged")
+    ? 0
+    : character.baseStats.PHY;
+
   return (
     <Tooltip
       key={weapon.name}
       content={
-        <div className="w-70 max-w-[calc(100vw-2rem)] text-center">
+        <div className="w-90 max-w-[calc(100vw-2rem)] text-center">
           <h3 className="mb-2 text-base font-normal text-yellow-300 underline">
             {weapon.name}
           </h3>
           <p className="mt-2 whitespace-pre-line">
+            <strong>Traits:</strong> {weapon.traits}
+          </p>
+          <p className="mt-2 whitespace-pre-line">
+            <strong>Damage:</strong> {weapon.dice + " " + weapon.damageType}
+          </p>
+          <p className="mt-2 whitespace-pre-line">
+            <strong>Hands:</strong> {weapon.hands}
+          </p>
+          <p className="mt-2 whitespace-pre-line">
+            <strong>Range:</strong> {weapon.range}
+          </p>
+          <p className="mt-2 whitespace-pre-line">
             <strong>Description:</strong> {weapon.description}
+          </p>
+          <p className="mt-2 whitespace-pre-line">
+            <strong>Price:</strong> {weapon.price}
+          </p>
+          <p className="mt-2 whitespace-pre-line">
+            <strong>Type:</strong> {weapon.type}
+          </p>
+          <p className="mt-2 whitespace-pre-line">
+            <strong>Weapon Group:</strong> {weapon.weaponGroup}
           </p>
         </div>
       }
@@ -68,6 +93,9 @@ export default function WeaponComponent({
               character.skillProficiencies[weapon.name] ?? "Trained",
             ).bonus
           })`}
+        </span>
+        <span>
+          {weapon.dice + " + " + damageBonus + " " + weapon.damageType}
         </span>
       </div>
     </Tooltip>
