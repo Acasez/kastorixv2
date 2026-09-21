a378f828a5457ec7c184aaf6fd7342386341bd20
Acasez <edvin.sanne@gmail.com>
2026-09-21 09:24:16 +0200

    Better spells and metamagic layout

--- src/components/CharacterSheet/ActionSection/MetamagicsSection.tsx
+++ src/components/CharacterSheet/ActionSection/MetamagicsSection.tsx
@@ -20,25 +20,28 @@ export default function MetamagicsSection({
     <div className="bg-gray-800 p-4 rounded-lg mt-3">
       <div className="flex justify-between items-center mb-4">
         <span className="text-lg underline">Metamagics</span>
-        <button
-          type="button"
-          className="bg-gray-600 text-white px-3 py-1 rounded"
-          onClick={() => onAddMetamagic()}
-        >
-          +
-        </button>
+        <div className="flex flex-row gap-2">
+          <p
+            className={`text-right ${
+              selectedMetamagics.length > maxKnownMetamagics
+                ? "text-red-500"
+                : selectedMetamagics.length < maxKnownMetamagics
+                  ? "text-green-500"
+                  : "text-text-light"
+            }`}
+          >
+            Metamagics Chosen ({selectedMetamagics.length}/{maxKnownMetamagics})
+          </p>
+          <button
+            type="button"
+            className="bg-gray-600 text-white px-3 py-1 rounded"
+            onClick={() => onAddMetamagic()}
+          >
+            +
+          </button>
+        </div>
       </div>
-      <p
-        className={`text-right ${
-          selectedMetamagics.length > maxKnownMetamagics
-            ? "text-red-500"
-            : selectedMetamagics.length < maxKnownMetamagics
-              ? "text-green-500"
-              : "text-text-light"
-        }`}
-      >
-        Metamagics Chosen ({selectedMetamagics.length}/{maxKnownMetamagics})
-      </p>
+
       <div className="flex flex-wrap gap-2 mt-3">
         {selectedMetamagics.map((metamagic) => (
           <MetamagicComponent
--- src/components/CharacterSheet/ActionSection/SpellRankSection.tsx
+++ src/components/CharacterSheet/ActionSection/SpellRankSection.tsx
@@ -33,29 +33,33 @@ export default function SpellRankSection({
     <div className="bg-gray-800 p-4 rounded-lg">
       <div className="flex justify-between items-center mb-4">
         <span className="text-lg underline">{rankName} Spells</span>
-        <button
-          type="button"
-          className="bg-gray-600 text-white px-3 py-1 rounded"
-          onClick={() => onAddSpell(rankName)}
-        >
-          +
-        </button>
-      </div>
-      <p>
-        DC {baseDC}, Complex DC {baseDC + 3}, Mana Cost {manaCost}
-      </p>
 
-      <p
-        className={`text-right ${
-          selectedSpells.length > maxKnownSpells
-            ? "text-red-500"
-            : selectedSpells.length < maxKnownSpells
-              ? "text-green-500"
-              : "text-text-light"
-        }`}
-      >
-        Selected Spells ({selectedSpells.length}/{maxKnownSpells})
-      </p>
+        <p>
+          DC {baseDC}, Complex DC {baseDC + 3}, Mana Cost {manaCost}
+        </p>
+        <div className="flex flex-row gap-2">
+          <p
+            className={`text-right ${
+              selectedSpells.length > maxKnownSpells
+                ? "text-red-500"
+                : selectedSpells.length < maxKnownSpells
+                  ? "text-green-500"
+                  : "text-text-light"
+            }`}
+          >
+            Selected Spells ({selectedSpells.length}/{maxKnownSpells})
+          </p>
+
+          <button
+            type="button"
+            className="bg-gray-600 text-white px-3 py-1 rounded"
+            onClick={() => onAddSpell(rankName)}
+          >
+            +
+          </button>
+        </div>
+      </div>
+
       <div className="flex flex-wrap gap-2 mt-3">
         {selectedSpells.map((spell) => (
           <SpellComponent
