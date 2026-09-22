77c092de85dec25038a3ddec18fb2cac6821bdf8
Acasez <edvin.sanne@gmail.com>
2026-09-22 19:19:38 +0200

    Styled the inventory a bit

--- src/components/CharacterSheet/TabbedSection/InventorySection.tsx
+++ src/components/CharacterSheet/TabbedSection/InventorySection.tsx
@@ -4,18 +4,22 @@ export default function InventorySection() {
   const { character, handleGoldChange } = useCharacter();
   return (
     <div className="p-4 text-white">
-      <h2 className="text-xl mb-2">Inventory Content</h2>
-      <p>This is the Inventory tab content.</p>
-      <div className="flex flex-row">
-        <p>Gold</p>
-        <input
-          type="number"
-          value={character.gold}
-          onChange={handleGoldChange}
-          min="0"
-          max="99999"
-          className="w-20 px-2 py-2 rounded-md border-2 border-blue-400 text-xl font-bold text-center bg-white focus:outline-none focus:ring-2 text-text-black focus:ring-blue-500"
-        />
+      <div className="flex flex-row justify-between">
+        <div>
+          <h2 className="text-xl mb-2">Inventory Content</h2>
+          <p>Here is the inventory</p>
+        </div>
+        <div className="flex items-center justify-center space-x-3 mt-2 p-1 bg-blue-200 rounded-lg border border-blue-400">
+          <span className="text-xl font-bold text-gray-800">Gold</span>
+          <input
+            type="number"
+            value={character.gold}
+            onChange={handleGoldChange}
+            min="0"
+            max="99999"
+            className="w-20 px-2 py-2 rounded-md border-2 border-blue-400 text-xl font-bold text-center bg-white focus:outline-none focus:ring-2 text-text-black focus:ring-blue-500"
+          />
+        </div>
       </div>
     </div>
   );
