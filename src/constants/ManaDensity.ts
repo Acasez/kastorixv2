cbab22f260fd9b347bc8232d8f6dfdb2774af45c
Acasez <edvin.sanne@gmail.com>
2026-09-25 15:08:56 +0200

    Mana Density Const

---
+++ src/constants/ManaDensity.ts
@@ -0,0 +1,37 @@
+export const MANA_DENSITY = [
+  {
+    name: "Null Zone",
+    hourlyRate: 5,
+    harnessManaDC: 18,
+    harnessMana: "1",
+    spellShapingModifier: -1,
+  },
+  {
+    name: "Low Mana",
+    hourlyRate: 10,
+    harnessManaDC: 15,
+    harnessMana: "1d4",
+    spellShapingModifier: 0,
+  },
+  {
+    name: "Normal",
+    hourlyRate: 20,
+    harnessManaDC: 14,
+    harnessMana: "1d6",
+    spellShapingModifier: 0,
+  },
+  {
+    name: "High",
+    hourlyRate: 30,
+    harnessManaDC: 12,
+    harnessMana: "1d8",
+    spellShapingModifier: 0,
+  },
+  {
+    name: "Potent",
+    hourlyRate: 45,
+    harnessManaDC: 10,
+    harnessMana: "1d10",
+    spellShapingModifier: 1,
+  },
+];
